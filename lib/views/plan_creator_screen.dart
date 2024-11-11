6cc4aff0dcf0050fc7fa6edb7bc0ce24e1046d80
Shinta Yulistiana <shintayulistiana2004@gmail.com>
2024-11-11 14:03:16 +0700

    up

--- lib/views/plan_creator_screen.dart
+++ lib/views/plan_creator_screen.dart
@@ -8,6 +8,26 @@ class PlanCreatorScreen extends StatefulWidget {
 }
 
 class _PlanCreatorScreenState extends State<PlanCreatorScreen> {
+  final textController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+// ganti ‘Namaku’ dengan nama panggilan Anda
+      appBar: AppBar(title: const Text('Master Plans Namaku')),
+      body: Column(children: [
+        _buildListCreator(),
+        Expanded(child: _buildMasterPlans())
+      ]),
+    );
+  }
+
+  @override
+  void dispose() {
+    textController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
